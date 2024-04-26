4c852addce6c53b0683a865bfea306c7d14881b4
Shailesh kale <shaileshkale87730@gmail.com>
2024-04-26 16:13:24 +0530

    add regstered user and bug fix

--- src/contrallers/user.contraller.js
+++ src/contrallers/user.contraller.js
@@ -1,9 +1,65 @@
+import { User } from "../models/User.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiError } from "../utils/AppError.js";
 import { asyncHandler } from "../utils/asycHandler.js";
+import { uploadincloudinary } from "../utils/cloudinary.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  await res.status(200).json({
-    message: "ok",
+  const { fullName, email, password, username } = req.body;
+  console.log(`register ${fullName} ${email} ${password} ${username} `);
+  /*first type check is Empty
+  if (fullName === "") {
+    throw new ApiError(400,"full name required");
+  }*/
+
+  /** 2nd type check to Empty */
+  if (
+    [fullName, email, password, username].some((field) => field.trim() === "")
+  ) {
+    throw new ApiError(400, "all fields must be required");
+  }
+
+  const existedUser = User.findOne({ $or: [{ username }, { email }] });
+
+  if (existedUser) {
+    throw new ApiError(409, "User with email or username already exists");
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar image is required");
+  }
+
+  const avatar = await uploadincloudinary(avatarLocalPath);
+  const coverImage = await uploadincloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar image is required");
+  }
+
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
   });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "somrthing went wrong ehile regstering the user");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "user regstered successfully"));
 });
 
 export { registerUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,21 @@
 import { Router } from "express";
 import { registerUser } from "../contrallers/user.contraller.js";
+import { upload } from "../middlewares/multer.niddlewares.js";
 
 const router = Router();
 // router's setup function router
-router.route("/register").post(registerUser);
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
 export default router;
