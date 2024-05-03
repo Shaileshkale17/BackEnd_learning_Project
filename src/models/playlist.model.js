e5530b09ce0fd3e0f5007212f01a7b85f67949ed
Shailesh kale <shaileshkale87730@gmail.com>
2024-05-03 12:38:20 +0530

    add commentthe playlist and tweet models

---
+++ src/models/playlist.model.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+
+const playlistSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    videos: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    owner: {
+      type: String,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const playList = mongoose.model("Playlist", playlistSchema);
